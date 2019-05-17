19162436750da45fd6dc7e7b9564f45b87d048d5
zhangxingzhong <744719042@qq.com>
2019-05-18 00:53:47 +0800

    调试网络测试用例

--- app/src/main/java/com/example/mynetwork/NetTestActivity.java
+++ app/src/main/java/com/example/mynetwork/NetTestActivity.java
@@ -1,12 +1,17 @@
 package com.example.mynetwork;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
@@ -14,18 +19,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class NetTestActivity extends AppCompatActivity {
     private static final String TAG = "NetTestActivity";
     private static Executor sExecutor = Executors.newFixedThreadPool(5);
-    private static final String HOST = "http://10.2.129.168:8080/HttpServer";
+    private static final String HOST = "http://192.168.137.240:8080/HttpServer";
+    private static final String FILE_NAME = "mount.jpg";
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net_test);
+        imageView = findViewById(R.id.image);
     }
 
     public void requestGet(View view) {
@@ -153,6 +162,7 @@ public class NetTestActivity extends AppCompatActivity {
                     while ((dataLen = data.read(buf)) != -1) {
                         outputStream.write(buf, 0, dataLen);
                     }
+                    outputStream.write("\r\n".getBytes("UTF-8"));
                     outputStream.write(("--" + boundary + "--").getBytes("UTF-8"));
                     outputStream.flush();
 
@@ -175,13 +185,14 @@ public class NetTestActivity extends AppCompatActivity {
         });
     }
 
+    final String DOWNLOAD_URL = HOST + "/download?filename=" + FILE_NAME;
+
     public void requestHead(View view) {
-        final String postUrl = HOST + "/download?filename=sport.mp4";
         sExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 try {
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(postUrl).openConnection();
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(DOWNLOAD_URL).openConnection();
                     httpURLConnection.setRequestMethod("HEAD");
                     httpURLConnection.setConnectTimeout(3000);
                     httpURLConnection.setReadTimeout(3000);
@@ -218,32 +229,34 @@ public class NetTestActivity extends AppCompatActivity {
     private volatile int fileSize = 0;
 
     public void downloadInterrupted(View view) {
-        final String postUrl = HOST + "/download";
+        final String DOWNLOAD_URL = HOST + "/download?filename=cat.jpg";
+        interrupt = false;
         sExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 try {
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(postUrl).openConnection();
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(DOWNLOAD_URL).openConnection();
                     httpURLConnection.setRequestMethod("GET");
                     httpURLConnection.setConnectTimeout(3000);
                     httpURLConnection.setReadTimeout(3000);
                     httpURLConnection.setDoInput(true);
-                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoOutput(false);
                     httpURLConnection.setInstanceFollowRedirects(true);
+                    httpURLConnection.addRequestProperty("Connection", "close");
 
                     if (fileSize > 0 && lastPos > 0) {
                         httpURLConnection.addRequestProperty("Range", "bytes=" + lastPos + "-" + fileSize);
                     }
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    outputStream.write("filename=sport.mp4".getBytes("UTF-8"));
-                    outputStream.flush();
 
                     httpURLConnection.getResponseCode();
                     printHeads(httpURLConnection.getHeaderFields());
                     httpURLConnection.getResponseCode();
                     fileSize = httpURLConnection.getContentLength();
                     InputStream inputStream = httpURLConnection.getInputStream();
-                    RandomAccessFile file = new RandomAccessFile(getCacheDir(), "video.mp4");
+                    RandomAccessFile file = new RandomAccessFile(getCacheDir().getAbsolutePath() + File.separator + "cat.jpg", "rw");
+                    if (lastPos > 0) {
+                        file.seek(lastPos);
+                    }
                     int len = -1;
                     byte[] buf = new byte[1024];
                     int pos = 0;
@@ -251,10 +264,24 @@ public class NetTestActivity extends AppCompatActivity {
                         file.write(buf, 0, len);
                         pos += len;
                         if (interrupt) {
+                            lastPos = pos + 1;
+                            Log.e(TAG, "interrupt at pos = " + lastPos);
                             return;
                         }
                     }
                     file.close();
+
+
+                    final File image = new File(getCacheDir(), "cat.jpg");
+                    if (image.length() == fileSize) {
+                        imageView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
                     httpURLConnection.disconnect();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -263,8 +290,12 @@ public class NetTestActivity extends AppCompatActivity {
         });
     }
 
+    public void interrupt(View view) {
+        interrupt = true;
+    }
+
     public void multiThreadDownload(View view) {
-        final String postUrl = HOST + "/download";
+        final String postUrl = HOST + "/download?filename=panda.jpg";
         sExecutor.execute(new Runnable() {
             @Override
             public void run() {
@@ -274,22 +305,19 @@ public class NetTestActivity extends AppCompatActivity {
                     httpURLConnection.setConnectTimeout(3000);
                     httpURLConnection.setReadTimeout(3000);
                     httpURLConnection.setDoInput(true);
-                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoOutput(false);
                     httpURLConnection.setInstanceFollowRedirects(true);
-
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    outputStream.write("filename=sport.mp4".getBytes("UTF-8"));
-                    outputStream.flush();
 
                     httpURLConnection.getResponseCode();
                     int contentLength = httpURLConnection.getContentLength();
                     httpURLConnection.disconnect();
 
+                    final CountDownLatch countDownLatch = new CountDownLatch(4);
                     int size = contentLength / 4;
                     for (int i = 0; i < 4; i++) {
                         final int start = size * i;
                         int end = start + size - 1;
-                        if (end > contentLength) {
+                        if (i == 3) {
                             end = contentLength;
                         }
 
@@ -304,16 +332,9 @@ public class NetTestActivity extends AppCompatActivity {
                                     httpURLConnection.setConnectTimeout(3000);
                                     httpURLConnection.setReadTimeout(3000);
                                     httpURLConnection.setDoInput(true);
-                                    httpURLConnection.setDoOutput(true);
+                                    httpURLConnection.setDoOutput(false);
                                     httpURLConnection.setInstanceFollowRedirects(true);
-
-                                    if (fileSize > 0 && lastPos > 0) {
-                                        httpURLConnection.addRequestProperty("Range", "bytes=" + startPos + "-" + endPos);
-                                    }
-                                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                                    outputStream.write("filename=sport.mp4".getBytes("UTF-8"));
-                                    outputStream.flush();
-
+                                    httpURLConnection.addRequestProperty("Range", "bytes:" + startPos + "-" + endPos);
                                     httpURLConnection.getResponseCode();
                                     printHeads(httpURLConnection.getHeaderFields());
                                     httpURLConnection.getResponseCode();
@@ -321,7 +342,7 @@ public class NetTestActivity extends AppCompatActivity {
                                     InputStream inputStream = httpURLConnection.getInputStream();
                                     int len = -1;
                                     byte[] buf = new byte[1024];
-                                    final RandomAccessFile file = new RandomAccessFile(getCacheDir(), "video1.mp4");
+                                    final RandomAccessFile file = new RandomAccessFile(getCacheDir().getAbsolutePath()  + File.separator + "panda.jpg", "rw");
                                     file.seek(startPos);
                                     while ((len = inputStream.read(buf)) != -1) {
                                         file.write(buf, 0, len);
@@ -331,9 +352,21 @@ public class NetTestActivity extends AppCompatActivity {
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
+
+                                countDownLatch.countDown();
                             }
-                        });
+                        }).start();
                     }
+
+                    countDownLatch.await();
+                    final File image = new File(getCacheDir(), "panda.jpg");
+                    imageView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -342,35 +375,42 @@ public class NetTestActivity extends AppCompatActivity {
     }
 
     public void httpTransferChuck(View view) {
-        final String postUrl = HOST + "/download";
         sExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 try {
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(postUrl).openConnection();
-                    httpURLConnection.setRequestMethod("HEAD");
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(DOWNLOAD_URL).openConnection();
+                    httpURLConnection.setRequestMethod("GET");
                     httpURLConnection.setConnectTimeout(3000);
                     httpURLConnection.setReadTimeout(3000);
                     httpURLConnection.setDoInput(true);
-                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoOutput(false);
                     httpURLConnection.setInstanceFollowRedirects(true);
-
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    outputStream.write("filename=sport.mp4".getBytes("UTF-8"));
-                    outputStream.flush();
 
                     httpURLConnection.getResponseCode();
                     printHeads(httpURLConnection.getHeaderFields());
+                    httpURLConnection.getResponseCode();
+                    fileSize = httpURLConnection.getContentLength(); // 返回-1
                     InputStream inputStream = httpURLConnection.getInputStream();
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    byte[] buf = new byte[1024];
+                    RandomAccessFile file = new RandomAccessFile(getCacheDir().getAbsolutePath() + File.separator + "mount.jpg", "rw");
                     int len = -1;
+                    byte[] buf = new byte[1024];
                     while ((len = inputStream.read(buf)) != -1) {
-                        bos.write(buf, 0, len);
+                        file.write(buf, 0, len);
                     }
+                    file.close();
 
-                    String text = new String(bos.toByteArray(), "UTF-8");
-                    Log.e(TAG, text);
+
+                    final File image = new File(getCacheDir(), "mount.jpg");
+//                    if (image.length() == fileSize) {
+                        imageView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+//                    }
                     httpURLConnection.disconnect();
                 } catch (Exception e) {
                     e.printStackTrace();
